dde23dfb5318b1a529a8420d48b23dfc0381cb1e
May <mayue1993925@qq.com>
2017-03-30 16:42:58 +0800

    2017-3-29 17:31:48 handle ins

--- server/user.js
+++ server/user.js
@@ -5,23 +5,23 @@ const sql = require('../MssqlHelper');
 let userServ = {};
 
 userServ.addUser = function (callback) {
-    const sqlString =
-        "INSERT  INTO dbo.student" +
-        "        ( SName, SGrade, SClass, SScore )" +
-        "VALUES  ( @name, @grade, @class, @score )";
-    let para = {
-        name: {sqlType: sql.sqlserver.NVarChar(20), inputValue: "John"},
-        grade: {sqlType: sql.sqlserver.Int, inputValue: "5"},
-        class: {sqlType: sql.sqlserver.Int, inputValue: "4"},
-        score: {sqlType: sql.sqlserver.Numeric(6, 2), inputValue: "88.74"}
-    };
-    sql.queryWithParams1(sqlString, para, function (error, recordsets, affected) {
-        if (error) {
-            return -1;
-        } else {
-            return affected;
-        }
-    })
+  let sqlString =
+    "INSERT  INTO dbo.student" +
+    "        ( SName, SGrade, SClass, SScore )" +
+    "VALUES  ( @name, @grade, @class, @score )";
+  let para = {
+    name: {sqlType: sql.sqlserver.NVarChar(20), inputValue: "John"},
+    grade: {sqlType: sql.sqlserver.Int, inputValue: "5"},
+    class: {sqlType: sql.sqlserver.Int, inputValue: "4"},
+    score: {sqlType: sql.sqlserver.Numeric(6, 2), inputValue: "88.74"}
+  };
+  sql.queryWithParams1(sqlString, para, function (error, recordsets, affected) {
+    if (error) {
+      return -1;
+    } else {
+      return affected;
+    }
+  })
 };
 
 module.exports = userServ;
